93f89a170bc81377d668b721a3d39ec7cfb1e7ab
Girish <girish.mahajan@yahoo.com>
2024-07-12 19:21:10 +0530

    Initial Checking

--- src/test/java/com/vois/consumer/iot/events/config/ApiDocumentationConfigTest.java
+++ src/test/java/com/vois/consumer/iot/events/config/ApiDocumentationConfigTest.java
@@ -1,4 +1,16 @@
+
 package com.vois.consumer.iot.events.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public class ApiDocumentationConfigTest {
+    @Test
+    public void testBasic() {
+        ApiDocumentationConfig apiDocumentationConfig = new ApiDocumentationConfig();
+        OpenAPI info = apiDocumentationConfig.openAPI();
+        assertNotNull(info);
+    }
 }
