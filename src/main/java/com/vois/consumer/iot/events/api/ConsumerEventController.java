210386ea9db8c5786a99bc4a33bfb63237977c05
Girish <girish.mahajan@yahoo.com>
2024-07-18 20:10:47 +0530

    Initial Checking

--- src/main/java/com/vois/consumer/iot/events/api/ConsumerEventController.java
+++ src/main/java/com/vois/consumer/iot/events/api/ConsumerEventController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
-import java.util.function.LongConsumer;
 
 @Controller
 public class ConsumerEventController {
